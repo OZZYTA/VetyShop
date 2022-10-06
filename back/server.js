749188a907fa9ebf9620dee5d45c7e4e89819138
Natali Velasquez <ozzyta1@gmail.com>
2022-10-06 06:39:57 -0500

    Creación del server

--- back/server.js
+++ back/server.js
@@ -1,1 +1,9 @@
-const app=require("app")
+const app=require("./app")
+
+//Setear el archivo de configuración
+const dotenv=require("dotenv");
+dotenv.config({path: 'back/config/config.env'})
+
+const server=app.listen(process.env.PORT, () => {
+    console.log(`Servidor iniciado en el puerto: ${process.env.PORT} en modo: ${process.env.NODE_ENV}`)
+})
