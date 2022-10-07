38f226e6650d438a65453234251f36943245542c
Natali Velasquez <ozzyta1@gmail.com>
2022-10-07 07:02:27 -0500

    Clase del 7 Octubre

---
+++ back/config/database.js
@@ -0,0 +1,12 @@
+const mongoose=require("mongoose");
+
+const connectDatabase = () => {
+    mongoose.connect(process.env.DB_LOCAL_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }).then(con => {
+        console.log(`Base de datos mongo conectada con el servidor: ${con.connection.host}`)
+    })
+}
+
+module.exports=connectDatabase;
--- back/server.js
+++ back/server.js
@@ -1,8 +1,12 @@
 const app=require("./app")
+const connectDatabase = require("./config/database");
 
 //Setear el archivo de configuración
 const dotenv=require("dotenv");
 dotenv.config({path: 'back/config/config.env'})
+
+//Configurar base de datos
+connectDatabase();
 
 //Llamemos al server
 const server=app.listen(process.env.PORT, () => {
