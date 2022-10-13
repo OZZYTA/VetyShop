013b34c1f444d4f48ad44629a4bc35a312defcf7
Natali Velasquez <ozzyta1@gmail.com>
2022-10-13 07:53:05 -0500

    Clase de Octubre 13

---
+++ front/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React from 'react';
+
+
+function App() {
+  return (
+    <div className="App">
+        <h1>Tienda de mascotas Vety Shop</h1>
+        <img src="./images/vetyshop.png" alt="Aqui va el logo"></img>
+    </div>
+  );
+}
+
+export default App;
