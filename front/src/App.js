a71eb427ac039f6a7961378174a13df10496656f
Natali Velasquez <ozzyta1@gmail.com>
2022-10-18 07:54:32 -0500

    Update total clase octubre 18

--- front/src/App.js
+++ front/src/App.js
@@ -2,23 +2,25 @@ import './App.css';
 import React from 'react';
 import Header from './components/layout/Header';
 import { Footer } from './components/layout/Footer';
+import Home from './components/Home';
+//Router traido desde react-router-dom (no confundir con el de express)
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 
 function App() {
   return (
+    <Router>
     <div className="App">
         <Header />
-        <center>Contenido aqui</center>
-        <center>Contenido aqui</center>
-        <center>Contenido aqui</center>
-        <center>Contenido aqui</center>
-        <center>Contenido aqui</center>
-        <center>Contenido aqui</center>
-        <center>Contenido aqui</center>
-        <center>Contenido aqui</center>
-        <center>Contenido aqui</center>
+        <div className='container container-fluid'>
+          <Routes>
+            <Route path="/" element={<Home />}/>
+            <Route path="/Home" element={<Home />}/>
+          </Routes>
+        </div>
         <Footer />
     </div>
+    </Router>
   );
 }
 
